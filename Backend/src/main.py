7fe4921e6753727d322cf092c536f60683b5eaeb
Nijamudeen96 <nijamudeen.96@gmail.com>
2021-03-14 13:10:46 +0800

    microcontroller endpoint

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,7 +10,19 @@ def hello():
 @app.route("/profile/<id>")
 def id_func(id):
     id = int(id)
-    return {"id":id*id}
+    return {"id":id, "id^2":id*id}
+
+#receive id, moisture, temperature, luminosity
+#send light and water duration
+@app.route("/input")
+def ESP32_Instructions():
+    my_id = request.args.get('id')
+    moisture = request.args.get('moisture')
+    temperature = request.args.get('temperature')
+    luminosity = request.args.get('luminosity')
+
+    return {"request":"ok"}
+
 
 if __name__ == "__main__":
     app.run(debug = True)
